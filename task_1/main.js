c175fafd67e1dd99d187fc239298df3e17ad0e21
Dansleym <Dansleymeplz@gmail.com>
2021-04-13 19:43:19 +0300

    js task 1

---
+++ task_1/main.js
@@ -0,0 +1,33 @@
+(function () {
+    class Calculator {
+        constructor() {}
+
+        sum(a,b) {
+            return a + b;
+        }
+
+        minus(a,b) {
+            return a - b;
+        }
+
+        multiplication(a,b) {
+            return a * b;
+        }
+
+        division(a,b) {
+            return b !== 0 ? a / b : "Нельзя делить на ноль!";
+        }
+
+        surplus(a,b) {
+            return  a % b;
+        }
+    }
+
+    let calculator = new Calculator();
+
+    console.log("Сумма 5 + 5 = " + calculator.sum(5,5));
+    console.log("Вычетание 5 - 5 = " + calculator.minus(5,5));
+    console.log("Умножение 5 * 5 = " + calculator.multiplication(5,5));
+    console.log("Деление 5 / 5 = " + calculator.division(5,5));
+    console.log("Остаток от деления 10 % 9 = " + calculator.surplus(10, 9));
+}());
